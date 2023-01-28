75cd59abee6586e83557879263fbadb604fe02bf
Éder Afonso Rabelo <eder.rabelo@proton.me>
2023-01-28 09:41:27 -0400

    Adicionando mudança de icones no dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
-document.querySelector("#btnMudarTema").addEventListener("click", () => {
-    document.body.classList.toggle("dark-theme")
+btnMudarTema = document.querySelector("#btnMudarTema");
+
+btnMudarTema.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+
+    console.log("click")
+    if (btnMudarTema.classList.value === "fa-solid fa-moon") {
+        console.log("ok")
+        btnMudarTema.classList.replace("fa-moon", "fa-sun");
+    } else {
+        btnMudarTema.classList.replace("fa-sun", "fa-moon");
+    }
 })
