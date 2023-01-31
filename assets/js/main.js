7c2da51ae20845f67364f91f7ca361d8cf764798
Éder Afonso Rabelo <eder.rabelo@proton.me>
2023-01-31 09:08:03 -0400

    Refatorando nome de classes e variaveis

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
-btnMudarTema = document.querySelector("#btnMudarTema");
+btnChangeTheme = document.querySelector("#btnChangeTheme");
 
-btnMudarTema.addEventListener("click", () => {
+btnChangeTheme.addEventListener("click", () => {
     document.body.classList.toggle("dark-theme");
 
     console.log("click")
-    if (btnMudarTema.classList.value === "fa-solid fa-moon") {
+    if (btnChangeTheme.classList.value === "fa-solid fa-moon") {
         console.log("ok")
-        btnMudarTema.classList.replace("fa-moon", "fa-sun");
+        btnChangeTheme.classList.replace("fa-moon", "fa-sun");
     } else {
-        btnMudarTema.classList.replace("fa-sun", "fa-moon");
+        btnChangeTheme.classList.replace("fa-sun", "fa-moon");
     }
 })
